4e0c16121d9424608a762d99c4e55072bfb26f20
Ada <adrianna.lesinska@gmail.com>
2026-08-19 10:31:29 +0200

    change input wrapper flex direction and clear script and style change in input

--- script.js
+++ script.js
@@ -8,37 +8,21 @@ const check = () => {
   console.log(value);
 
   if (value === "") {
-    result.innerHTML = `Enter a valid word!`;
+    result.innerHTML = `Enter a word!`;
     input.value = "";
   } else if (value === value.split("").reverse().join("") && value !== "") {
-    // console.log("ok");
     result.innerHTML = `${value} is a palindrome 🙂​`;
     input.value = "";
   } else {
-    console.log("nie ok");
     result.innerHTML = `${value} is not a palindrome ​🤔​`;
     input.value = "";
   }
-
-  // if (value === "") {
-  //   result.innerHTML = `Enter a word‼️`;
-  //   input.value = "";
-  // } else if (value === value.split("").reverse().join("")) {
-  //   console.log("ok");
-  //   result.innerHTML = `${value} is a palindrome 🙂​`;
-  //   input.value = "";
-  // } else {
-  //   console.log("nie ok");
-  //   result.innerHTML = `${value} is not a palindrome ​🤔​`;
-  //   input.value = "";
-  // }
 };
 
 const clear = () => {
   input.value = "";
   result.innerHTML = "​";
 };
-// console.log(result);
 
 const enterKeyCheck = (e) => {
   if (e.key === "Enter") {
@@ -50,6 +34,4 @@ const enterKeyCheck = (e) => {
 
 checkBtn.addEventListener("click", check);
 clearBtn.addEventListener("click", clear);
-// clearBtn.addEventListener("click", check);
 input.addEventListener("keyup", enterKeyCheck);
-// input.addEventListener("keyup", enterKeyCheck);
